d4bca6c5d93394af89d6e08c659a259f54d0c664
Nevenit <Nevenit@outlook.com>
2022-05-10 16:12:08 +1000

    Changed flow of code. Added ability to change order of the videos and subtitles

---
+++ OtherFunctions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TvShowSubtitleRenamer
+{
+    public class OtherFunctions
+    {
+        public static void RearrangeList(ref string[] arr, int[] order)
+        {
+            string[] final = new string[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                final[i] = arr[order[i]];
+            }
+
+            arr = final;
+        }
+
+        public static int[] AskOrder()
+        {
+            Console.WriteLine("What is the correct order (eg. \"1,3,2,4,5,6,7,8\")");
+            string input = Console.ReadLine();
+            
+            string[] splitList = input.Split(',');
+            int[] intList = new int[splitList.Length];
+
+            for (int i = 0; i < splitList.Length; i++)
+            {
+                try
+                {
+                    intList[i] = Int16.Parse(splitList[i]);
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect format, please try again.");
+                    AskOrder();
+                }
+            }
+            
+            // Verify List
+            List<int> arrList = intList.ToList();
+            for (int i = 0; i < arrList.Count - 2; i++)
+            {
+                if (arrList[i + 1] - arrList[i] != 1)
+                {
+                    Console.WriteLine("A number seems to be missing, please try again.");
+                    AskOrder();
+                }
+            }
+
+            return intList;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,14 @@ namespace TvShowSubtitleRenamer
 {
     internal class Program
     {
+        private static string _showPath;
+        private static string[] _videoPaths;
+        private static string[] _subtitlePaths;
+        private static string language;
+        
         public static void Main(string[] args)
         {
+            /*
             while (true)
             {
                 string pathToShow = "";
@@ -60,6 +66,79 @@ namespace TvShowSubtitleRenamer
                 Console.WriteLine("Process complete, press any key to exit.");
                 Console.ReadKey();
             }
+            */
+        }
+        
+        void GetPath()
+        {
+            Console.Write("Path to the shows directory (containing episodes and subtitles):");
+            _showPath = Console.ReadLine();
+            if (Directory.Exists(_showPath))
+            {
+                // Find all mp4 and srt files
+                _videoPaths = Directory.GetFiles(_showPath, "*.*").Where(s => Path.GetExtension(s) == ".mp4").ToArray();
+                _subtitlePaths = Directory.GetFiles(_showPath, "*.*").Where(s => Path.GetExtension(s) == ".srt").ToArray();
+                
+                // Sort arrays to get the right order
+                Array.Sort(_videoPaths);
+                Array.Sort(_subtitlePaths);
+
+                CheckVideoOrder();
+            }
+            else
+                Console.WriteLine("This path doesn't exist, try again.");
+        }
+        
+        void CheckVideoOrder()
+        {
+            // List all mp4 files
+            for (int i = 0; i < _videoPaths.Length; i++)
+                Console.WriteLine((i + 1) + ". " + Path.GetFileName(_videoPaths[i]));
+
+            Console.WriteLine("Are these the correct video files, in the right order? (y/n):");
+            string answerV = Console.ReadLine();
+            if (answerV.ToLower()[0] != 'y')
+                CheckSubtitleOrder();
+            else
+            {
+                int[] correctOrder = OtherFunctions.AskOrder();
+                OtherFunctions.RearrangeList(ref _videoPaths, correctOrder);
+                CheckVideoOrder();
+            }
+                
+        }
+
+        void CheckSubtitleOrder()
+        {
+            // List all srt files
+            for (int i = 0; i < _subtitlePaths.Length; i++)
+                Console.WriteLine((i + 1) + ". " + Path.GetFileName(_subtitlePaths[i]));
+
+            Console.WriteLine("Are these the correct video files, in the right order? (y/n):");
+            string answerS = Console.ReadLine();
+            if (answerS.ToLower()[0] != 'y')
+                ChooseLanguage();
+            else
+            {
+                int[] correctOrder = OtherFunctions.AskOrder();
+                OtherFunctions.RearrangeList(ref _subtitlePaths, correctOrder);
+                CheckSubtitleOrder();
+            }
+        }
+
+        void ChooseLanguage()
+        {
+            Console.Write("Subtitle language (eg. en): ");
+            language = Console.ReadLine();
+        }
+
+        void FinalFunction()
+        {
+            for (int i = 0; i < _subtitlePaths.Length; i++)
+                File.Move(_subtitlePaths[i], _showPath + "\\" + Path.GetFileNameWithoutExtension(_videoPaths[i]) + "." + language + ".srt");
+
+            Console.WriteLine("Process complete, press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
